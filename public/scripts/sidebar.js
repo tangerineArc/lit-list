0e659bf2ae19397368df8b8ad195d329575f5d06
Swagatam <swagatamheroz@outlook.com>
2025-03-01 20:38:48 +0530

    feat: create header ui

--- public/scripts/sidebar.js
+++ public/scripts/sidebar.js
@@ -1,9 +1,9 @@
 (() => {
-  function listToggler(trigger, status, contentBox) {
+  function listToggler(trigger, openStatus, contentBox) {
     const chevron = trigger.querySelector(".chevron");
 
     trigger.addEventListener("click", () => {
-      if (status) {
+      if (openStatus) {
         contentBox.className = "closing-box";
         chevron.classList.add("rotate-box");
       } else {
@@ -11,7 +11,7 @@
         chevron.classList.remove("rotate-box");
       }
 
-      status = !status;
+      openStatus = !openStatus;
     });
   }
 
@@ -26,4 +26,28 @@
     true,
     document.querySelector("#add-new-list")
   );
+})();
+
+(() => {
+  const sidebarCloseButton = document.querySelector("#sidebar-close-button");
+  const sidebarOpenButton = document.querySelector("#sidebar-open-button");
+
+  const sidebar = document.querySelector("aside");
+
+  sidebarOpenButton.style.display = "none";
+
+  if (document.documentElement.clientWidth < 1200) {
+    sidebar.style.display = "none";
+    sidebarOpenButton.style.display = "inline";
+  }
+
+  sidebarCloseButton.addEventListener("click", () => {
+    sidebar.style.display = "none";
+    sidebarOpenButton.style.display = "inline";
+  });
+
+  sidebarOpenButton.addEventListener("click", () => {
+    sidebar.style.display = "flex";
+    sidebarOpenButton.style.display = "none";
+  });
 })();
